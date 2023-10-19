e2595e7b4cac211b7461e2cf639c1e2739321b5d
Joel Filho <joelfilho02812@gmail.com>
2023-10-19 09:52:59 -0300

    project created

---
+++ src/main/java/br/com/joelbrs/JFCatalog/JfCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelbrs.JFCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joelbrs/JFCatalog/JfCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelbrs.JFCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
